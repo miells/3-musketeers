bd63419a96b1b615d4b3ad9251f439a074b06d85
Muriel LIN-SI <miells974@gmail.com>
2019-03-04 17:47:58 +0100

    starwars tests implemented

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -7,25 +7,29 @@ describe('starwars-names', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
     test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      for(var i=0; i<starWars.all.length; i++)
+      {
+        expect(typeof starWars.all[i]).toBe('string');
+      }
     });
 
     test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toContain('Luke Skywalker');
     });
 
     test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).not.toContain('Ben Quadinaros');
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toContain(starWars.random());
     });
 
     test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      var number = 5;
+      expect(starWars.random(number)).toHaveLength(number);
     });
   });
 });
